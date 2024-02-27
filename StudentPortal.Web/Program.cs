57fb092db08bc5cf117c8964ff897f9f219f69a3
atash <atash@LAPTOP-R2LDVLRR>
2024-02-27 17:25:09 -0600

    Add new routing pattern for Product controller
    A new controller route pattern has been added specifically for the Product controller. This pattern is named "productRoute" and its primary function is to simplify the handling of actions related to viewing products.

--- StudentPortal.Web/Program.cs
+++ StudentPortal.Web/Program.cs
@@ -24,4 +24,10 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapControllerRoute(
+    name: "productRoute",
+    pattern: "Product/{action=ViewProduct}/{productId?}");
+
 app.Run();
+
+
