f0b4345dff96672c293235a9a8b83c86884ca566
atash <atash@LAPTOP-R2LDVLRR>
2024-02-28 21:16:57 -0600

    Update StudentPortal and remove ConsoleApp1
    Removed the ConsoleApp1 project from the solution and updated the StudentPortal solution. The update includes addition of new packages (CsvHelper, Microsoft.NET.Test.Sdk, Newtonsoft.Json, NUnit) in StudentPortal.Web, creation of a new folder 'resources', and implementation of a new test class 'test1.cs' for CSV to JSON conversion.

---
+++ StudentPortal.Web/test1.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+using CsvHelper;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace StudentPortal.Web
+{
+    [TestFixture]
+    public class TestClass1
+    {
+        [Test]
+        public void CsvToJson()
+        {
+            // read the csv file
+            var csvFile = File.ReadAllText(Path.Combine("Resources", "StudentData.csv"));
+        
+            using(var csvReader = new CsvReader(new StringReader(csvFile), CultureInfo.InvariantCulture))
+            {
+                var records = csvReader.GetRecords<Student>().ToList();
+            
+                // convert the rows to json
+                var json = JsonConvert.SerializeObject(records);
+            
+                // save the json to a file
+                File.WriteAllText(Path.Combine("Resources", "StudentData.json"), json);
+            }
+        }
+    }
+
+    public class Student
+    {
+        // define Student properties here
+    }
+}
+
+
