7e42d9276387d67d270e47715cc8ffff43b4a10b
atash <atash@LAPTOP-R2LDVLRR>
2024-03-03 01:14:31 -0600

    Add JSON deserialization to HomeController
    In the HomeController, a new method 'HelloWorld' was added that accepts a JSON string as a parameter. This JSON string is deserialized into an object using the Newtonsoft.Json library, which was added into this file. The method further extracts a value from the deserialized object to display in the response.

--- StudentPortal.Web/Controllers/HomeController.cs
+++ StudentPortal.Web/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using StudentPortal.Web.Models;
 using System.Diagnostics;
+using Newtonsoft.Json;
 
 namespace StudentPortal.Web.Controllers
 {
@@ -23,6 +24,21 @@ namespace StudentPortal.Web.Controllers
         public IActionResult HelloWorld()
         {
             return Ok("Hello, world!");
+        }
+        
+        // TODO 1: fix this.
+        [HttpPost]
+        [Route("api/customroute")] // Change the route name here
+        public IActionResult HelloWorld([FromBody] string jsonString)
+        {
+            // Deserialize the JSON string into an object
+            dynamic jsonObject = JsonConvert.DeserializeObject(jsonString);
+
+            // Extract the value you want to display
+            string value = jsonObject.value;
+
+            // Return the response
+            return Ok($"Hello, {value}!");
         }
 
 
